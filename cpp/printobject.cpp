e1fb3d8e1c90eb2d3fa78dc1855eae26d24201b2
Øystein Walle <oystwa@gmail.com>
2019-10-25 14:49:05 +0200

    No zlib wrapper

--- cpp/printobject.cpp
+++ cpp/printobject.cpp
@@ -1,29 +1,30 @@
-#include "zstr.hpp"
-
 #include <fmt/core.h>
+#include <zlib.h>
 
 #include <algorithm>
-#include <array>
-#include <iostream>
-#include <iterator>
+#include <cstdio>
 #include <string_view>
 #include <vector>
 
 int main(int argc, char const *argv[])
 {
-    zstr::istreambuf zsbuf(std::cin.rdbuf(), 1<<16, true);
-    std::istream is(&zsbuf);
-    is.exceptions(std::ios_base::badbit);
+    std::vector<unsigned char> input(1024, '\0');
 
-    std::string obj_data{std::istreambuf_iterator<char>(is), std::istreambuf_iterator<char>()};
+    auto size = std::fread(input.data(), 1, 1024, stdin);
+    input.resize(size);
+
+    size_t len = 2048;
+    std::vector<unsigned char> obj_data(len, '\0');
+    ::uncompress(obj_data.data(), &len, input.data(), input.size());
+    obj_data.resize(len);
 
     auto space = std::find(obj_data.begin(), obj_data.end(), ' ');
     auto null = std::find(space, obj_data.end(), '\0');
 
-    std::string_view type(obj_data.data(), std::distance(obj_data.begin(), space));
+    std::string_view type(reinterpret_cast<char*>(obj_data.data()), std::distance(obj_data.begin(), space));
 
     auto content_len = obj_data.size() - std::distance(obj_data.begin(), null) - 1;
-    std::string_view content(obj_data.data() + std::distance(obj_data.begin(), null + 1), content_len);
+    std::string_view content(reinterpret_cast<char*>(obj_data.data()) + std::distance(obj_data.begin(), null + 1), content_len);
 
     fmt::print("Type: {}\nContent:\n{}", type, content);
 
